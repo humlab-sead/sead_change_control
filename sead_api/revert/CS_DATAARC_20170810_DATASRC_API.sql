01918a78eb64956217e7ee2d9eafa997a3caea4e
roger-mahler <roger.mahler@hotmail.com>
2019-04-12 15:31:16 +0200

    Added sead_api project

---
+++ sead_api/revert/CS_DATAARC_20170810_DATASRC_API.sql
@@ -0,0 +1,7 @@
+-- Revert sead_api:CS_DATAARC_20170810_DATASRC_API from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ sead_api/verify/CS_DATAARC_20170810_DATASRC_API.sql
@@ -0,0 +1,7 @@
+-- Verify sead_api:CS_DATAARC_20170810_DATASRC_API on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
