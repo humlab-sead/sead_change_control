3659bdc5fc921d71646e0757fdf923be80a29de1
Roger Mähler <roger.mahler@hotmail.com>
2019-10-12 19:19:33 +0200

    Added new tasks

---
+++ general/verify/20191012_DML_ASSOCIATION_TYPE_UPDATES.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20191012_DML_ASSOCIATION_TYPE_UPDATES on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
---
+++ sead_api/verify/20191012_DML_ECOCODE_GEOJSON_EXPORT.sql
@@ -0,0 +1,7 @@
+-- Verify sead_api:20191012_DML_ECOCODE_GEOJSON_EXPORT on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
