b8bc72ed44cd80bb6e410ae1bc2683d195d9292c
Roger Mähler <roger.mahler@hotmail.com>
2019-10-12 17:11:59 +0200

    Initial commit of isotope data model

---
+++ general/revert/20191012_DDL_ISOTOPE_MODEL.sql
@@ -0,0 +1,7 @@
+-- Revert sead_change_control:20191012_DDL_ISOTOPE_MODEL from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ general/verify/20191012_DDL_ISOTOPE_MODEL.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20191012_DDL_ISOTOPE_MODEL on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
