16a20806327bedab1ebe1910f9b6da6277f109e3
Roger Mähler <roger.mahler@hotmail.com>
2019-10-14 11:50:39 +0200

    Resolves #16. Resolves humlab-sead/issues/2

---
+++ general/revert/20191014_DML_DATASET_NULL_MASTER_ID.sql
@@ -0,0 +1,7 @@
+-- Revert sead_change_control:20191014_DML_DATASET_NULL_MASTER_ID from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ general/verify/20191014_DML_DATASET_NULL_MASTER_ID.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20191014_DML_DATASET_NULL_MASTER_ID on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
