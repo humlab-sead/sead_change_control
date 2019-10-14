04c261a59a1b2333054ca6aca1c47a07ff72ecdd
roger-mahler <roger.mahler@hotmail.com>
2019-10-14 18:25:01 +0200

    Removed guard

--- general/deploy/20180612_DDL_DENDRO_ALTER_DENDRO_DATES_ALLOW_NULL.sql
+++ general/deploy/20180612_DDL_DENDRO_ALTER_DENDRO_DATES_ALLOW_NULL.sql
@@ -1,5 +1,3 @@
--- Deploy sead_db_change_control:CSR_20190408_ALTER_DENDRO_DATES_ALLOW_NULL to pg
-
 /****************************************************************************************************************
   Change author
     Roger Mähler, 2018-06-12
@@ -25,39 +23,6 @@
     Changes must be propagated to Clearing House
 *****************************************************************************************************************/
 
--- Deploy sead_db_change_control:CSA_20180607_ALTER_SAMPLE_GROUP_SAMPLING_CONTEXT to pg
-
-/****************************************************************************************************************
-  Author
-  Date
-  Description
-  Prerequisites
-  Reviewer
-  Approver
-  Idempotent    Yes
-  Notes
-*****************************************************************************************************************/
-
 begin;
-do $$
-begin
-    begin
-
-        if (select count(*)
-            from INFORMATION_SCHEMA.COLUMNS
-            where table_schema = 'public'
-              and table_name = 'tbl_dendro_dates'
-              and column_name = 'error_uncertainty_id'
-              and is_nullable = 'YES') = 1
-        then
-            raise exception sqlstate 'GUARD';
-        end if;
-
-        alter table public.tbl_dendro_dates alter column error_uncertainty_id drop not null;
-
-    exception when sqlstate 'GUARD' then
-        raise notice 'ALREADY EXECUTED';
-    end;
-
-end $$;
+    alter table public.tbl_dendro_dates alter column error_uncertainty_id drop not null;
 commit;
