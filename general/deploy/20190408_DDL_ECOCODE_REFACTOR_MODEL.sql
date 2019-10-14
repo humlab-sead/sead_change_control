67bf05cb8a6dbdcb0b263e7fb6db416804c57f06
roger-mahler <roger.mahler@hotmail.com>
2019-10-14 18:19:06 +0200

    Removed guard

--- general/deploy/20190408_DDL_ECOCODE_REFACTOR_MODEL.sql
+++ general/deploy/20190408_DDL_ECOCODE_REFACTOR_MODEL.sql
@@ -14,32 +14,25 @@
 begin;
 do $$
 begin
-    begin
+    set client_min_messages to warning;
 
-        set client_min_messages to warning;
+    drop index if exists tbl_ecocode_groups_idx_label;
+    drop index if exists tbl_ecocode_groups_idx_name;
 
-        if sead_utility.column_exists('public'::text, 'tbl_ecocode_definitions'::text, 'name'::text) = TRUE then
-            raise exception sqlstate 'GUARD';
-        end if;
+    if sead_utility.column_exists('public'::text, 'tbl_ecocode_definitions'::text, 'label'::text) = TRUE then
+        alter table tbl_ecocode_definitions rename column "label" to "name";
+    end if;
 
-        drop index if exists tbl_ecocode_groups_idx_label;
-        drop index if exists tbl_ecocode_groups_idx_name;
+    alter table tbl_ecocode_groups
+        drop column if exists "label",
+        alter column "name" drop not null,
+        alter column "name" type character varying(200);
 
-        if sead_utility.column_exists('public'::text, 'tbl_ecocode_groups'::text, 'label'::text) = TRUE then
-            alter table tbl_ecocode_definitions rename column "label" to "name";
-        end if;
-
+    if sead_utility.column_exists('public'::text, 'tbl_ecocode_groups'::text, 'abbreviation'::text) = FALSE then
         alter table tbl_ecocode_groups
-            drop column if exists "label",
-            alter column "name" drop not null,
-            alter column "name" type character varying(200),
             add column "abbreviation" character varying(255);
+    end if;
 
-        create index tbl_ecocode_groups_idx_name on tbl_ecocode_groups using btree (name);
-
-    exception when sqlstate 'GUARD' then
-        raise notice 'ALREADY EXECUTED';
-    end;
-
+    create index tbl_ecocode_groups_idx_name on tbl_ecocode_groups using btree (name);
 end $$;
 commit;
