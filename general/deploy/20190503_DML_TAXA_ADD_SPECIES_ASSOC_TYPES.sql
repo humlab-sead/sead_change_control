3745268873896b8d67fb8d2900ddc7dbc0305125
Roger Mähler <roger.mahler@hotmail.com>
2019-10-30 09:27:10 +0100

    Duplicate assoc type removed

--- general/deploy/20190503_DML_TAXA_ADD_SPECIES_ASSOC_TYPES.sql
+++ general/deploy/20190503_DML_TAXA_ADD_SPECIES_ASSOC_TYPES.sql
@@ -31,7 +31,17 @@ begin
                 ('is kleptoparasitic on'),
                 ('is kleptoparasitised by'),
                 ('is loosely associated with'),
-                ('predated on by')
+                ('predated on by'),
+		        ('in same streams as'),
+		        ('in same ponds as'),
+		        ('is found with'),
+		        ('is predated in nests by'),
+		        ('in similar woodland pools as'),
+		        ('in same log as'),
+		        ('pupae parasitized by?'),
+		        ('inquiline with?'),
+		        ('on burnt ground with'),
+		        ('is endoparasite of pupae of')
         )
         insert into tbl_species_association_types (association_type_name)
             select association_type_name
@@ -39,6 +49,14 @@ begin
             left join tbl_species_association_types x using (association_type_name)
             where x.association_type_id is null
             order by association_type_name;
+
+        update tbl_species_association_types set association_type_name = 'has undefined association with' where association_type_name = 'undefined association with';
+        update tbl_species_association_types set association_type_name = 'inquiline with' where association_type_name = 'inquiline';
+        update tbl_species_association_types set association_type_name = 'found around nests of' where association_type_name = 'around nests of';
+        update tbl_species_association_types set association_type_name = 'swarms with' where association_type_name = 'Swarming';
+        update tbl_species_association_types set association_type_name = 'is cuckoo parasite in burrows of' where association_type_name = 'cuckoo parasite in burrows of';
+        update tbl_species_association_types set association_type_name = 'is semi-parasitic with' where association_type_name = 'semi-parasitic with';
+        update tbl_species_association_types set association_type_name = 'is parasite of' where association_type_name = 'parasite of';
 
     exception when sqlstate 'GUARD' then
         raise notice 'ALREADY EXECUTED';
--- general/deploy/20191012_DML_ASSOCIATION_TYPE_UPDATES.sql
+++ general/deploy/20191012_DML_ASSOCIATION_TYPE_UPDATES.sql
@@ -16,26 +16,8 @@ do $$
 begin
 
     begin
+        raise notice 'DEPRECATED: Moved to 20190503_DML_TAXA_ADD_SPECIES_ASSOC_TYPES';
 
-        update tbl_species_association_types set association_type_name = 'has undefined association with' where association_type_name = 'undefined association with';
-        update tbl_species_association_types set association_type_name = 'inquiline with' where association_type_name = 'inquiline';
-        update tbl_species_association_types set association_type_name = 'found around nests of' where association_type_name = 'around nests of';
-        update tbl_species_association_types set association_type_name = 'swarms with' where association_type_name = 'Swarming';
-        update tbl_species_association_types set association_type_name = 'is cuckoo parasite in burrows of' where association_type_name = 'cuckoo parasite in burrows of';
-        update tbl_species_association_types set association_type_name = 'is semi-parasitic with' where association_type_name = 'semi-parasitic with';
-        update tbl_species_association_types set association_type_name = 'is parasite of' where association_type_name = 'parasite of';
-
-        insert into tbl_species_association_types (association_type_name, association_description) values ('in same streams as', NULL);
-        insert into tbl_species_association_types (association_type_name, association_description) values ('in same ponds as', NULL);
-        insert into tbl_species_association_types (association_type_name, association_description) values ('is found with', NULL);
-        insert into tbl_species_association_types (association_type_name, association_description) values ('is predated in nests by', NULL);
-        insert into tbl_species_association_types (association_type_name, association_description) values ('in similar woodland pools as', NULL);
-        insert into tbl_species_association_types (association_type_name, association_description) values ('in same upland habitat as', NULL);
-        insert into tbl_species_association_types (association_type_name, association_description) values ('in same log as', NULL);
-        insert into tbl_species_association_types (association_type_name, association_description) values ('pupae parasitized by?', NULL);
-        insert into tbl_species_association_types (association_type_name, association_description) values ('inquiline with?', NULL);
-        insert into tbl_species_association_types (association_type_name, association_description) values ('on burnt ground with', NULL);
-        insert into tbl_species_association_types (association_type_name, association_description) values ('is endoparasite of pupae of', NULL);
 
     exception when sqlstate 'GUARD' then
         raise notice 'ALREADY EXECUTED';
