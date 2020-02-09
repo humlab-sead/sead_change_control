7ed9170643a53db1a6402f332b4a81beff5e0dc9
Roger Mähler <roger.mahler@hotmail.com>
2020-02-09 13:36:53 +0100

    Resolves #45

---
+++ general/revert/20200210_DML_MAL_QOS_UPDATE_01.sql
@@ -0,0 +1,7 @@
+-- Revert sead_change_control:20200210_DML_MAL_QOS_UPDATE_01 from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ general/verify/20200210_DML_MAL_QOS_UPDATE_01.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20200210_DML_MAL_QOS_UPDATE_01 on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
