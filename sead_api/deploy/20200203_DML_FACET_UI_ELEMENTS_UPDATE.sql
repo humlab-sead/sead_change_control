07205705f8385ba3ecc9c4dc95c7f0c29930ab35
Roger Mähler <roger.mahler@hotmail.com>
2020-02-09 13:46:43 +0100

    Resolves #37 (part of)

--- sead_api/deploy/20200203_DML_FACET_UI_ELEMENTS_UPDATE.sql
+++ sead_api/deploy/20200203_DML_FACET_UI_ELEMENTS_UPDATE.sql
@@ -15,19 +15,13 @@ begin;
 do $$
 begin
 
-    begin
-
-        update facet.facet set display_title = 'Magnetic sus.', description = 'Magnetic sus.' where facet_code = 'tbl_denormalized_measured_values_33_0';
-        update facet.facet set display_title = 'Loss of Ignition', description = 'Loss of Ignition' where facet_code = 'tbl_denormalized_measured_values_32';
-        update facet.facet set display_title = 'Phosphates', description = 'Phosphates' where facet_code = 'tbl_denormalized_measured_values_37';
-        update facet.facet set display_title = 'Taxon' where facet_code = 'species';
-        update facet.facet set display_title = 'Countries' where facet_code = 'country';
-        update facet.facet set display_title = 'Site' where facet_code = 'site';
-        update facet.facet set display_title = 'Sample groups' where facet_code = 'sample_groups';
-
-    exception when sqlstate 'GUARD' then
-        raise notice 'ALREADY EXECUTED';
-    end;
+    update facet.facet set display_title = 'Magnetic sus.', description = 'Magnetic sus.' where facet_code = 'tbl_denormalized_measured_values_33_0';
+    update facet.facet set display_title = 'Loss of Ignition', description = 'Loss of Ignition' where facet_code = 'tbl_denormalized_measured_values_32';
+    update facet.facet set display_title = 'Phosphates', description = 'Phosphates' where facet_code = 'tbl_denormalized_measured_values_37';
+    update facet.facet set display_title = 'Taxon' where facet_code = 'species';
+    update facet.facet set display_title = 'Countries' where facet_code = 'country';
+    update facet.facet set display_title = 'Site' where facet_code = 'site';
+    update facet.facet set display_title = 'Sample groups' where facet_code = 'sample_groups';
 
 end $$;
 commit;
