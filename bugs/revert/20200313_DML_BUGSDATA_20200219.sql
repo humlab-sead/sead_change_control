c03e98d4e4d8d6f7a5a4fca3a8b532f683ce4db0
Roger Mähler <roger.mahler@hotmail.com>
2020-03-13 15:45:17 +0100

    BugsCEP import diff @2020.02 issue #36

---
+++ bugs/revert/20200313_DML_BUGSDATA_20200219.sql
@@ -0,0 +1,7 @@
+-- Revert bugs:20200313_DML_BUGSDATA_20200219 from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ bugs/verify/20200313_DML_BUGSDATA_20200219.sql
@@ -0,0 +1,7 @@
+-- Verify bugs:20200313_DML_BUGSDATA_20200219 on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
