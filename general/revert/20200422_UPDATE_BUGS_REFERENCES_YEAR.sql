1e8c5f2a015b8ff470ac536a1ce807969693d98f
Roger Mähler <roger.mahler@hotmail.com>
2020-04-22 17:55:26 +0200

    Resolves #69

---
+++ general/revert/20200422_UPDATE_BUGS_REFERENCES_YEAR.sql
@@ -0,0 +1,7 @@
+-- Revert sead_change_control:20200422_UPDATE_BUGS_REFERENCES_YEAR from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ general/verify/20200422_UPDATE_BUGS_REFERENCES_YEAR.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20200422_UPDATE_BUGS_REFERENCES_YEAR on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
