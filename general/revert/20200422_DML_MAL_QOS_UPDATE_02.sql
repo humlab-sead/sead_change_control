00fa27878c8aa515f9b27cc0abd08a2e37b59f5c
Roger Mähler <roger.mahler@hotmail.com>
2020-04-22 16:36:09 +0200

    Resolves #66

---
+++ general/revert/20200422_DML_MAL_QOS_UPDATE_02.sql
@@ -0,0 +1,7 @@
+-- Revert sead_change_control:20200422_DML_MAL_QOS_UPDATE_02 from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ general/verify/20200422_DML_MAL_QOS_UPDATE_02.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20200422_DML_MAL_QOS_UPDATE_02 on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
