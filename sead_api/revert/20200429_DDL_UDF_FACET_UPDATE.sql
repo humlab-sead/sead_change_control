0c5cdadaef6f0b433ef64c6455292c3430c999f0
Roger Mähler <roger.mahler@hotmail.com>
2020-04-30 10:24:31 +0200

    Resolves #77

---
+++ sead_api/revert/20200429_DDL_UDF_FACET_UPDATE.sql
@@ -0,0 +1,7 @@
+-- Revert sead_api:20200429_DDL_UDF_FACET_UPDATE from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ sead_api/verify/20200429_DDL_UDF_FACET_UPDATE.sql
@@ -0,0 +1,7 @@
+-- Verify sead_api:20200429_DDL_UDF_FACET_UPDATE on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
