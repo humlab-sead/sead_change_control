5ad77e7c0f92ccbc4009ca14c4638e95056c183c
Roger Mähler <roger.mahler@hotmail.com>
2022-11-21 10:45:44 +0100

    Dendro lookups standardization and update (#89)

---
+++ general/revert/20221121_DML_DENDRO_LOOKUP_STANDARD_UPDATE.sql
@@ -0,0 +1,7 @@
+-- Revert sead_change_control:20221121_DML_DENDRO_LOOKUP_STANDARD_UPDATE from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ general/verify/20221121_DML_DENDRO_LOOKUP_STANDARD_UPDATE.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20221121_DML_DENDRO_LOOKUP_STANDARD_UPDATE on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
