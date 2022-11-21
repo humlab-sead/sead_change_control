822a7a3c8f4720dfb62d640206c7cdc267ef8878
Roger Mähler <roger.mahler@hotmail.com>
2022-11-21 10:52:21 +0100

    Add analysis methods for dendro humlab-sead/sead_general_database/#4

---
+++ general/revert/20221121_DML_DENDRO_ADD_ANALYSIS_METHODS.sql
@@ -0,0 +1,7 @@
+-- Revert sead_change_control:20221121_DML_DENDRO_ADD_ANALYSIS_METHODS from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ general/verify/20221121_DML_DENDRO_ADD_ANALYSIS_METHODS.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20221121_DML_DENDRO_ADD_ANALYSIS_METHODS on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
