ee8b56ff20fd097a93e3a9c880e9aca8268770f7
Roger Mähler <roger.mahler@hotmail.com>
2022-11-21 12:52:40 +0100

    Generate REST APU  utility UDF

---
+++ utility/revert/20221118_DDL_RESTAPI_GENERATE_SCHEMA.sql
@@ -0,0 +1,7 @@
+-- Revert utility:20221118_DDL_RESTAPI_GENERATE_SCHEMA from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ utility/verify/20221118_DDL_RESTAPI_GENERATE_SCHEMA.sql
@@ -0,0 +1,7 @@
+-- Verify utility:20221118_DDL_RESTAPI_GENERATE_SCHEMA on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
