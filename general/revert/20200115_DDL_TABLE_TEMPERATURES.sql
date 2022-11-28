3f827bbcfdf248c8de0f4ffc9620594dab9e421c
Roger Mähler <roger.mahler@hotmail.com>
2022-11-28 15:48:59 +0100

    Added temperature table

---
+++ general/revert/20200115_DDL_TABLE_TEMPERATURES.sql
@@ -0,0 +1,7 @@
+-- Revert sead_change_control:20200115_DDL_TABLE_TEMPERATURES from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ general/verify/20200115_DDL_TABLE_TEMPERATURES.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20200115_DDL_TABLE_TEMPERATURES on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
