2caa8464a476c9185351d5cf62fe961032533b39
Roger Mähler <roger.mahler@hotmail.com>
2022-12-02 11:44:50 +0100

    Simplified script

--- submissions/deploy/20191221_DML_SUBMISSION_BUGS_20190303_POST_APPLY.sql
+++ submissions/deploy/20191221_DML_SUBMISSION_BUGS_20190303_POST_APPLY.sql
@@ -12,43 +12,10 @@
 *****************************************************************************************************************/
 
 begin;
-
-create or replace function bugs_import.post_import_updates() returns void language plpgsql
-as $$
-begin
-
-    insert into tbl_sample_alt_refs (alt_ref, physical_sample_id, alt_ref_type_id)
-    	select bugs_identifier, sead_reference_id, 12
-    	from bugs_import.bugs_trace b
-    	where bugs_table = 'TSample'
-    	  on conflict (sample_alt_ref_id) do update
-    	 	set alt_ref = excluded.alt_ref,
-    			physical_sample_id = excluded.physical_sample_id;
-
-    update tbl_ecocode_groups set ecocode_system_id = 3 where abbreviation = 'Eco';
-    update tbl_ecocode_groups set ecocode_system_id = 3 where abbreviation = 'FCo';
-    update tbl_ecocode_groups set ecocode_system_id = 3 where abbreviation = 'FDe';
-    update tbl_ecocode_groups set ecocode_system_id = 3 where abbreviation = 'FNo';
-    update tbl_ecocode_groups set ecocode_system_id = 3 where abbreviation = 'HRa';
-    update tbl_ecocode_groups set ecocode_system_id = 3 where abbreviation = 'HTy';
-
-    insert into tbl_dataset_submissions (dataset_id, submission_type_id, contact_id, date_submitted, notes)
-    	select d.dataset_id, 5, 1, d.date_updated, 'Single dataset from another database submission into SEAD'
-    	from tbl_datasets d
-    	left join tbl_dataset_submissions s using (dataset_id)
-    	where TRUE
-    	  and s.dataset_submission_id is null
-    	  and d.master_set_id = 1;
-
-
-    update tbl_ecocode_definitions set ecocode_group_id = 2 where ecocode_group_id is null;
-
-end;
-$$;
-
 do $$
 begin
-    perform bugs_import.post_import_updates();
-end $$ language plpgsql;
 
+    perform bugs_import.post_import_updates();
+
+end $$;
 commit;
