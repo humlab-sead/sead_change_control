a4230e28253eb9ded8ecb00defa0286117ad22ef
Roger Mähler <roger.mahler@hotmail.com>
2022-12-06 15:17:32 +0100

    Fixed part of #81

---
+++ general/revert/20221205_DML_QUALITY_CONTROL_BIBLIO_UPDATES.sql
@@ -0,0 +1,7 @@
+-- Revert sead_change_control:20221205_DML_QUALITY_CONTROL_BIBLIO_UPDATES from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ general/verify/20221205_DML_QUALITY_CONTROL_BIBLIO_UPDATES.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20221205_DML_QUALITY_CONTROL_BIBLIO_UPDATES on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
