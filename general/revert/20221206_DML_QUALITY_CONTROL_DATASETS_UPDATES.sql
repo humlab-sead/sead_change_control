1d95e5c1133590b0672bdc9e0dd0a6aa513f4250
Roger Mähler <roger.mahler@hotmail.com>
2022-12-07 08:59:06 +0100

    Resolves #81

---
+++ general/revert/20221206_DML_QUALITY_CONTROL_DATASETS_UPDATES.sql
@@ -0,0 +1,7 @@
+-- Revert sead_change_control:20221206_DML_QUALITY_CONTROL_DATASETS_UPDATES from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ general/verify/20221206_DML_QUALITY_CONTROL_DATASETS_UPDATES.sql
@@ -0,0 +1,7 @@
+-- Verify sead_change_control:20221206_DML_QUALITY_CONTROL_DATASETS_UPDATES on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
