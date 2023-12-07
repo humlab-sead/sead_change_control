a07d90529d68ee5e0acbd3b421390dec99dcf498
Roger Mähler <roger.mahler@hotmail.com>
2023-12-07 14:54:16 +0100

    https://github.com/humlab-sead/sead_change_control/issues/154

---
+++ sead_api/revert/20231207_DML_FACET_GRAPH_UPDATE.sql
@@ -0,0 +1,7 @@
+-- Revert sead_api:20231207_DML_FACET_GRAPH_UPDATE from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ sead_api/verify/20231207_DML_FACET_GRAPH_UPDATE.sql
@@ -0,0 +1,7 @@
+-- Verify sead_api:20231207_DML_FACET_GRAPH_UPDATE on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
