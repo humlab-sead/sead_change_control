71e18f7851078b9d99a5951099c075eb0f5ee742
Roger Mähler <roger.mahler@hotmail.com>
2023-12-18 14:36:51 +0100

    Changed to update by name instead of ID.

--- ceramics/deploy/20221205_DML_CERAMICS_METHOD_ID_UPDATE.sql
+++ ceramics/deploy/20221205_DML_CERAMICS_METHOD_ID_UPDATE.sql
@@ -15,14 +15,43 @@ begin;
 do $$
 begin
 
-    begin
 
-        update tbl_ceramics_lookup set method_id = 171 where method_id = 70;
-        update tbl_ceramics_lookup set method_id = 172 where method_id = 71;
-
-    exception when sqlstate 'GUARD' then
-        raise notice 'ALREADY EXECUTED';
-    end;
+    update tbl_ceramics_lookup
+        set method_id = 172
+    where name in (
+		'Firing temperature(min)',
+		'Melting point', 'Firing atmosphere'
+	  ) and method_id <> 172;
+            
+    update tbl_ceramics_lookup
+        set method_id = 171
+    where name in (
+        'Clay: Coarseness',
+        'Clay: Sorting',
+        'Clay: Silt',
+        'Clay: Sand',
+        'Clay: Calcarbon',
+        'Clay: Ironoxide',
+        'Clay: Limonite',
+        'Clay: Mica',
+        'Clay: Fossil',
+        'Clay: Spongie',
+        'Clay: Diatome', 
+        'Clay: Organic', 
+        'Clay: Ore', 
+        'Tempering: Granite', 
+        'Tempering: Rock', 
+        'Tempering: Sand',
+        'Tempering: Chamotte', 
+        'Tempering: Asbestos', 
+        'Tempering: Calcarbon', 
+        'Tempering: Bone', 
+        'Tempering: Organic', 
+        'Tempering: Natural', 
+        'Tempering: Accessory', 
+        'Tempering: Max Grain Size',
+        '% - Tempering'
+    ) and method_id <> 171;
 
 end $$;
 commit;
