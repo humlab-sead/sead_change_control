1b3cc075277fc490b6b17c3f3aa2be28c09641a4
Roger Mähler <roger.mahler@hotmail.com>
2023-12-19 14:39:13 +0100

    Changes to clearinghouse schema related to #214

--- subsystem/deploy/20191217_DDL_CLEARINGHOUSE_SYSTEM.sql
+++ subsystem/deploy/20191217_DDL_CLEARINGHOUSE_SYSTEM.sql
@@ -25,7 +25,7 @@ grant execute on all functions in schema public, sead_utility to clearinghouse_w
 
 alter default privileges in schema public, sead_utility
 grant all privileges on tables to clearinghouse_worker;
-alter default privileges in schema public, sead_utility
+alter default privileges in schema public, sead_utility;
 grant all privileges on sequences to clearinghouse_worker;
 
 set role clearinghouse_worker;
@@ -979,7 +979,7 @@ This is an auto-generated mail from the SEAD Clearing House system.
                 set description = excluded.description;
 
     insert into clearing_house.tbl_clearinghouse_user_roles (role_id, role_name)
-        values (0, 'Undefined'),
+        values  (0, 'Undefined'),
                 (1, 'Reader'),
                 (2, 'Normal'),
                 (3, 'Administrator'),
@@ -997,14 +997,170 @@ This is an auto-generated mail from the SEAD Clearing House system.
                 ('mattias_admin', '$2y$10$/u3RCeK8Q.2s75UsZmvQ4.4TOxvLNKH8EoH4k6NYYtkAMavjP.dry', 'Mattias Sjölander', 3, 3, '2013-10-08', 'mattias.sjolander@umu.se', true)
         on conflict do nothing;
 
+    with sead_tables (table_id, table_name, table_name_underscored) as (values
+        (1, 'TblSampleTypes', 'tbl_sample_types'),
+        (2, 'TblErrorUncertainties', 'tbl_error_uncertainties'),
+        (3, 'TblSampleGroupReferences', 'tbl_sample_group_references'),
+        (4, 'TblTaxonomyNotes', 'tbl_taxonomy_notes'),
+        (5, 'TblPhysicalSampleFeatures', 'tbl_physical_sample_features'),
+        (6, 'TblRecordTypes', 'tbl_record_types'),
+        (7, 'TblAgeTypes', 'tbl_age_types'),
+        (8, 'TblDatasetSubmissions', 'tbl_dataset_submissions'),
+        (9, 'TblEcocodeDefinitions', 'tbl_ecocode_definitions'),
+        (10, 'TblLocations', 'tbl_locations'),
+        (11, 'TblDendroDateNotes', 'tbl_dendro_date_notes'),
+        (12, 'TblCeramicsLookup', 'tbl_ceramics_lookup'),
+        (13, 'TblTaxonomicOrderBiblio', 'tbl_taxonomic_order_biblio'),
+        (14, 'TblTextBiology', 'tbl_text_biology'),
+        (15, 'TblTextDistribution', 'tbl_text_distribution'),
+        (16, 'TblAnalysisEntityDimensions', 'tbl_analysis_entity_dimensions'),
+        (17, 'TblSampleNotes', 'tbl_sample_notes'),
+        (18, 'TblSampleGroupDimensions', 'tbl_sample_group_dimensions'),
+        (19, 'TblAbundanceElements', 'tbl_abundance_elements'),
+        (20, 'TblSampleDescriptionTypes', 'tbl_sample_description_types'),
+        (21, 'TblDataTypeGroups', 'tbl_data_type_groups'),
+        (22, 'TblUnits', 'tbl_units'),
+        (23, 'TblDatasetSubmissionTypes', 'tbl_dataset_submission_types'),
+        (24, 'TblAggregateDatasets', 'tbl_aggregate_datasets'),
+        (25, 'TblSampleGroupDescriptionTypes', 'tbl_sample_group_description_types'),
+        (26, 'TblChronControlTypes', 'tbl_chron_control_types'),
+        (27, 'TblSampleLocationTypes', 'tbl_sample_location_types'),
+        (28, 'TblDatingMaterial', 'tbl_dating_material'),
+        (29, 'TblTaxaMeasuredAttributes', 'tbl_taxa_measured_attributes'),
+        (30, 'TblRdb', 'tbl_rdb'),
+        (31, 'TblImportedTaxaReplacements', 'tbl_imported_taxa_replacements'),
+        (32, 'TblAbundances', 'tbl_abundances'),
+        (33, 'TblIdentificationLevels', 'tbl_identification_levels'),
+        (34, 'TblContactTypes', 'tbl_contact_types'),
+        (35, 'TblTaxonomicOrder', 'tbl_taxonomic_order'),
+        (36, 'TblSiteImages', 'tbl_site_images'),
+        (37, 'TblEcocodeSystems', 'tbl_ecocode_systems'),
+        (38, 'TblTephraRefs', 'tbl_tephra_refs'),
+        (39, 'TblTaxaImages', 'tbl_taxa_images'),
+        (40, 'TblFeatures', 'tbl_features'),
+        (41, 'TblTephraDates', 'tbl_tephra_dates'),
+        (42, 'TblActivityTypes', 'tbl_activity_types'),
+        (43, 'TblProjects', 'tbl_projects'),
+        (44, 'TblDendro', 'tbl_dendro'),
+        (45, 'TblMcrNames', 'tbl_mcr_names'),
+        (46, 'TblAltRefTypes', 'tbl_alt_ref_types'),
+        (47, 'TblIsotopes', 'tbl_isotopes'),
+        (48, 'TblMethodGroups', 'tbl_method_groups'),
+        (49, 'TblAbundanceModifications', 'tbl_abundance_modifications'),
+        (50, 'TblMethods', 'tbl_methods'),
+        (51, 'TblDatasetMethods', 'tbl_dataset_methods'),
+        (52, 'TblDendroMeasurements', 'tbl_dendro_measurements'),
+        (53, 'TblYearsTypes', 'tbl_years_types'),
+        (54, 'TblModificationTypes', 'tbl_modification_types'),
+        (55, 'TblTaxaReferenceSpecimens', 'tbl_taxa_reference_specimens'),
+        (56, 'TblAggregateSamples', 'tbl_aggregate_samples'),
+        (57, 'TblSiteLocations', 'tbl_site_locations'),
+        (58, 'TblTaxaCommonNames', 'tbl_taxa_common_names'),
+        (59, 'TblTaxaTreeGenera', 'tbl_taxa_tree_genera'),
+        (60, 'TblTaxaSynonyms', 'tbl_taxa_synonyms'),
+        (61, 'TblDatingLabs', 'tbl_dating_labs'),
+        (62, 'TblSiteOtherRecords', 'tbl_site_other_records'),
+        (63, 'TblSampleGroupCoordinates', 'tbl_sample_group_coordinates'),
+        (64, 'TblChronologies', 'tbl_chronologies'),
+        (65, 'TblSampleImages', 'tbl_sample_images'),
+        (66, 'TblSitePreservationStatus', 'tbl_site_preservation_status'),
+        (67, 'TblDatasetMasters', 'tbl_dataset_masters'),
+        (68, 'TblSampleGroupSamplingContexts', 'tbl_sample_group_sampling_contexts'),
+        (69, 'TblSampleLocations', 'tbl_sample_locations'),
+        (70, 'TblSampleCoordinates', 'tbl_sample_coordinates'),
+        (71, 'TblIsotopeMeasurements', 'tbl_isotope_measurements'),
+        (72, 'TblSampleGroupDescriptionTypeSamplingContexts', 'tbl_sample_group_description_type_sampling_contexts'),
+        (73, 'TblImageTypes', 'tbl_image_types'),
+        (74, 'TblRdbCodes', 'tbl_rdb_codes'),
+        (75, 'TblChronControls', 'tbl_chron_controls'),
+        (76, 'TblRdbSystems', 'tbl_rdb_systems'),
+        (77, 'TblTephras', 'tbl_tephras'),
+        (78, 'TblTaxaTreeFamilies', 'tbl_taxa_tree_families'),
+        (79, 'TblPhysicalSamples', 'tbl_physical_samples'),
+        (80, 'TblHorizons', 'tbl_horizons'),
+        (81, 'TblLithology', 'tbl_lithology'),
+        (82, 'TblIsotopeValueSpecifiers', 'tbl_isotope_value_specifiers'),
+        (83, 'TblCeramics', 'tbl_ceramics'),
+        (84, 'TblSpeciesAssociationTypes', 'tbl_species_association_types'),
+        (85, 'TblMeasuredValueDimensions', 'tbl_measured_value_dimensions'),
+        (86, 'TblSampleLocationTypeSamplingContexts', 'tbl_sample_location_type_sampling_contexts'),
+        (87, 'TblAbundanceIdentLevels', 'tbl_abundance_ident_levels'),
+        (88, 'TblProjectStages', 'tbl_project_stages'),
+        (89, 'TblLanguages', 'tbl_languages'),
+        (90, 'TblSampleGroupNotes', 'tbl_sample_group_notes'),
+        (91, 'TblCeramicsMeasurements', 'tbl_ceramics_measurements'),
+        (92, 'TblMcrSummaryData', 'tbl_mcr_summary_data'),
+        (93, 'TblDatasets', 'tbl_datasets'),
+        (94, 'TblSampleHorizons', 'tbl_sample_horizons'),
+        (95, 'TblFeatureTypes', 'tbl_feature_types'),
+        (96, 'TblIsotopeStandards', 'tbl_isotope_standards'),
+        (97, 'TblAnalysisEntityPrepMethods', 'tbl_analysis_entity_prep_methods'),
+        (98, 'TblTaxaTreeAuthors', 'tbl_taxa_tree_authors'),
+        (99, 'TblEcocodes', 'tbl_ecocodes'),
+        (100, 'TblDendroLookup', 'tbl_dendro_lookup'),
+        (101, 'TblMcrdataBirmbeetledat', 'tbl_mcrdata_birmbeetledat'),
+        (102, 'TblTaxonomicOrderSystems', 'tbl_taxonomic_order_systems'),
+        (103, 'TblSampleDimensions', 'tbl_sample_dimensions'),
+        (104, 'TblSiteNatgridrefs', 'tbl_site_natgridrefs'),
+        (105, 'TblDatingUncertainty', 'tbl_dating_uncertainty'),
+        (106, 'TblDendroDates', 'tbl_dendro_dates'),
+        (107, 'TblSampleDescriptions', 'tbl_sample_descriptions'),
+        (108, 'TblGeochronRefs', 'tbl_geochron_refs'),
+        (109, 'TblSampleDescriptionSampleGroupContexts', 'tbl_sample_description_sample_group_contexts'),
+        (110, 'TblDimensions', 'tbl_dimensions'),
+        (111, 'TblSeasonOrQualifier', 'tbl_season_or_qualifier'),
+        (112, 'TblIsotopeTypes', 'tbl_isotope_types'),
+        (113, 'TblEcocodeGroups', 'tbl_ecocode_groups'),
+        (114, 'TblDatasetContacts', 'tbl_dataset_contacts'),
+        (115, 'TblAggregateSampleAges', 'tbl_aggregate_sample_ages'),
+        (116, 'TblAggregateOrderTypes', 'tbl_aggregate_order_types'),
+        (117, 'TblSeasonTypes', 'tbl_season_types'),
+        (118, 'TblAnalysisEntities', 'tbl_analysis_entities'),
+        (119, 'TblRelativeAgeTypes', 'tbl_relative_age_types'),
+        (120, 'TblAnalysisEntityAges', 'tbl_analysis_entity_ages'),
+        (121, 'TblSampleAltRefs', 'tbl_sample_alt_refs'),
+        (122, 'TblSampleGroupDescriptions', 'tbl_sample_group_descriptions'),
+        (123, 'TblSampleGroups', 'tbl_sample_groups'),
+        (124, 'TblCoordinateMethodDimensions', 'tbl_coordinate_method_dimensions'),
+        (125, 'TblMeasuredValues', 'tbl_measured_values'),
+        (126, 'TblGeochronology', 'tbl_geochronology'),
+        (127, 'TblSiteReferences', 'tbl_site_references'),
+        (128, 'TblDataTypes', 'tbl_data_types'),
+        (129, 'TblSampleColours', 'tbl_sample_colours'),
+        (130, 'TblTaxaSeasonality', 'tbl_taxa_seasonality'),
+        (131, 'TblRelativeAges', 'tbl_relative_ages'),
+        (132, 'TblTextIdentificationKeys', 'tbl_text_identification_keys'),
+        (133, 'TblSpeciesAssociations', 'tbl_species_associations'),
+        (134, 'TblBiblio', 'tbl_biblio'),
+        (135, 'TblTaxaTreeMaster', 'tbl_taxa_tree_master'),
+        (136, 'TblRelativeAgeRefs', 'tbl_relative_age_refs'),
+        (137, 'TblSampleGroupImages', 'tbl_sample_group_images'),
+        (138, 'TblRelativeDates', 'tbl_relative_dates'),
+        (139, 'TblContacts', 'tbl_contacts'),
+        (140, 'TblSites', 'tbl_sites'),
+        (141, 'TblProjectTypes', 'tbl_project_types'),
+        (142, 'TblUpdatesLog', 'tbl_updates_log'),
+        (143, 'TblColours', 'tbl_colours'),
+        (144, 'TblSeasons', 'tbl_seasons'),
+        (145, 'TblTaxaTreeOrders', 'tbl_taxa_tree_orders'),
+        (146, 'TblLocationTypes', 'tbl_location_types')
+    ) insert into clearing_house.tbl_clearinghouse_submission_tables (table_id, table_name, table_name_underscored)
+        select table_id, table_name, table_name_underscored
+        from sead_tables;
+
+    perform sead_utility.sync_sequences('clearing_house', 'tbl_clearinghouse_submission_tables', 'table_id');
+
     with sead_tables as (
         Select distinct table_name
         From clearing_house.fn_dba_get_sead_public_db_schema('public', 'sead_master')
     ) insert into clearing_house.tbl_clearinghouse_submission_tables (table_name, table_name_underscored)
         select replace(initcap(replace(table_name, '_', ' ')), ' ', '') , table_name
         from sead_tables
-        where table_name Like 'tbl_%'
-      on conflict (table_name) do nothing;
+        left join clearing_house.tbl_clearinghouse_submission_tables x
+          using (table_name)
+        where TRUE
+          and table_name Like 'tbl_%'
+          and x.table_id is not null;
 
     insert into clearing_house.tbl_clearinghouse_submission_states (submission_state_id, submission_state_name)
         values	(0, 'Undefined'),
@@ -1545,34 +1701,43 @@ call clearing_house.create_public_model(false, false);
 ******************************************************************************************************************************/
 -- Select clearing_house.fn_delete_submission(4)
 
-create or replace function clearing_house.fn_delete_submission(p_submission_id int, p_clear_header boolean=FALSE, p_clear_exploded boolean=TRUE)
-Returns void As $$
-    Declare v_table_name_underscored character varying;
-Begin
+create or replace function clearing_house.fn_delete_submission(p_submission_id int, p_clear_header boolean=false, p_clear_exploded boolean=true)
+returns void as $$
+    declare v_table_name_underscored character varying;
+begin
 
-    If p_clear_exploded Then
-        Delete From clearing_house.tbl_clearinghouse_submission_xml_content_values Where submission_id = p_submission_id;
-        Delete From clearing_house.tbl_clearinghouse_submission_xml_content_columns Where submission_id = p_submission_id;
-        Delete From clearing_house.tbl_clearinghouse_submission_xml_content_records Where submission_id = p_submission_id;
-        Delete From clearing_house.tbl_clearinghouse_submission_xml_content_tables Where submission_id = p_submission_id;
-    End If;
+    if p_clear_exploded then
+        delete from clearing_house.tbl_clearinghouse_submission_xml_content_values where submission_id = p_submission_id;
+        delete from clearing_house.tbl_clearinghouse_submission_xml_content_columns where submission_id = p_submission_id;
+        delete from clearing_house.tbl_clearinghouse_submission_xml_content_records where submission_id = p_submission_id;
+        delete from clearing_house.tbl_clearinghouse_submission_xml_content_tables where submission_id = p_submission_id;
+    end if;
 
-    For v_table_name_underscored in (
-        Select table_name_underscored
-        From clearing_house.tbl_clearinghouse_submission_tables
-    ) Loop
-        -- Raise Notice 'Table %...', v_table_name_underscored;
-        Execute format('Delete From clearing_house.%s Where submission_id = %s;', v_table_name_underscored, p_submission_id);
-    End Loop;
+    for v_table_name_underscored in (
+        select table_name_underscored
+        from clearing_house.tbl_clearinghouse_submission_tables
+    ) loop
 
-    If p_clear_header Then
-        Delete From clearing_house.tbl_clearinghouse_submissions Where submission_id = p_submission_id;
-        Perform setval(pg_get_serial_sequence('clearing_house.tbl_clearinghouse_submissions', 'submission_id'), coalesce(max(submission_id), 0) + 1, false)
-        From clearing_house.tbl_clearinghouse_submissions;
-    End If;
+      if exists (
+        select 1
+        from pg_tables
+        where schemaname = 'clearing_house'
+          and tablename  = v_table_name_underscored
+      ) then
+          raise notice 'table %...', v_table_name_underscored;
+          execute format('delete from clearing_house.%s where submission_id = %s;', v_table_name_underscored, p_submission_id);
+        end if;
+    end loop;
 
-    -- Raise Notice 'Done!';
-End $$ Language plpgsql;
+    if p_clear_header then
+        delete from clearing_house.tbl_clearinghouse_submissions where submission_id = p_submission_id;
+        perform setval(pg_get_serial_sequence('clearing_house.tbl_clearinghouse_submissions', 'submission_id'), coalesce(max(submission_id), 0) + 1, false)
+        from clearing_house.tbl_clearinghouse_submissions;
+    end if;
+
+    -- raise notice 'done!';
+end $$ language plpgsql;
+
 
 /*****************************************************************************************************************************
 **	Function	fn_get_submission_table_column_names
