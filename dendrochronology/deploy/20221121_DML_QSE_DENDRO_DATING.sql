fa5d0d3aaa86380c09f4d6ac5646b381aa0511f8
Roger Mähler <roger.mahler@hotmail.com>
2023-12-19 07:19:57 +0100

    Revised model (#135)

--- dendrochronology/deploy/20221121_DML_QSE_DENDRO_DATING.sql
+++ dendrochronology/deploy/20221121_DML_QSE_DENDRO_DATING.sql
@@ -1,6 +1,5 @@
 -- Deploy dendrochronology: 20221121_DML_QSE_DENDRO_DATING
-
-/****************************************************************************************************************
+ /****************************************************************************************************************
   Author        Roger Mähler
   Date          2019-01-01
   Description
@@ -9,50 +8,38 @@
   Approver
   Idempotent    Yes
   Notes
-*****************************************************************************************************************/
+*****************************************************************************************************************/ begin;
 
-begin;
 do $$
 begin
+    -- FIXME: Breaking change
+    drop view if exists postgrest_api.qse_dendro_dating;
 
-    begin
-
-        drop view if exists "postgrest_api"."qse_dendro_dating";
-
-        create or replace view "postgrest_api"."qse_dendro_dating" as
+    create or replace view postgrest_api.qse_dendro_dating as
         select distinct ps.physical_sample_id,
             ae.analysis_entity_id,
-            dl.name AS date_type,
-            ps.sample_name AS sample,
-            at.age_type,
-            dd.age_older AS older,
-            dd.age_younger AS younger,
-            dd.error_plus AS plus,
-            dd.error_minus AS minus,
-            eu.error_uncertainty_type AS error_uncertainty,
-            soq.season_or_qualifier_type AS season
-        from ((((((tbl_physical_samples ps
-            join tbl_analysis_entities ae on ((ps.physical_sample_id = ae.physical_sample_id)))
-            join tbl_dendro_dates dd on ((ae.analysis_entity_id = dd.analysis_entity_id)))
-            left join tbl_season_or_qualifier soq on ((soq.season_or_qualifier_id = dd.season_or_qualifier_id)))
-            left join tbl_age_types at on ((at.age_type_id = dd.age_type_id)))
-            left join tbl_error_uncertainties eu on ((eu.error_uncertainty_id = dd.error_uncertainty_id)))
-            left join tbl_dendro_lookup dl on ((dd.dendro_lookup_id = dl.dendro_lookup_id)));
+            dl.name as "date_type",
+            ps.sample_name as "sample",
+            t.age_type as "age_type",
+            dd.age_older as "older",
+            dd.age_younger as "younger",
+            s.season_name as "season"
+            -- dd.error_plus AS plus,
+            -- dd.error_minus AS minus,
+            -- eu.error_uncertainty_type AS error_uncertainty,
+            -- soq.season_or_qualifier_type AS season
+        from tbl_physical_samples ps
+        join tbl_analysis_entities ae on ps.physical_sample_id = ae.physical_sample_id
+        join tbl_dendro_dates dd on ae.analysis_entity_id = dd.analysis_entity_id
+        left join tbl_seasons s using ("season_id")
+        left join tbl_age_types t using ("age_type_id")
+        left join tbl_dendro_lookup dl using ("dendro_lookup_id");
 
-        alter view if exists "postgrest_api"."qse_dendro_dating" owner to postgrest;
-        grant select on table "postgrest_api"."qse_dendro_dating" to humlab_admin; --warn: grant\revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-        grant insert on table "postgrest_api"."qse_dendro_dating" to humlab_admin; --warn: grant\revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-        grant update on table "postgrest_api"."qse_dendro_dating" to humlab_admin; --warn: grant\revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-        grant delete on table "postgrest_api"."qse_dendro_dating" to humlab_admin; --warn: grant\revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-        grant truncate on table "postgrest_api"."qse_dendro_dating" to humlab_admin; --warn: grant\revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-        grant references on table "postgrest_api"."qse_dendro_dating" to humlab_admin; --warn: grant\revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
-        grant trigger on table "postgrest_api"."qse_dendro_dating" to humlab_admin; --warn: grant\revoke privileges to a role can occure in a sql error during execution if role is missing to the target database!
+    alter view if exists postgrest_api.qse_dendro_dating owner to postgrest;
 
-        comment on view "postgrest_api"."qse_dendro_dating"  is null;
+    grant all privileges on table postgrest_api.qse_dendro_dating to humlab_admin;
 
-    exception when sqlstate 'GUARD' then
-        raise notice 'ALREADY EXECUTED';
-    end;
+    comment on view postgrest_api.qse_dendro_dating  is null;
 
 end $$;
 commit;
