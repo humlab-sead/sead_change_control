080744419e057df98d95c68c7c7d034c1bead356
Roger Mähler <roger.mahler@hotmail.com>
2023-12-19 10:30:18 +0100

    Moved lookups to own CR (#160, #213)

---
+++ sead_api/revert/20190101_DML_FACETS_LOOKUP.sql
@@ -0,0 +1,7 @@
+-- Revert sead_api:20190101_DML_FACETS_LOOKUP from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ sead_api/verify/20190101_DML_FACETS_LOOKUP.sql
@@ -0,0 +1,7 @@
+-- Verify sead_api:20190101_DML_FACETS_LOOKUP on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
