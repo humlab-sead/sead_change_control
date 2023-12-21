44426dd986de50ab3eaa846baa3cccefba83b8ab
Roger Mähler <roger.mahler@hotmail.com>
2023-12-21 23:55:02 +0100

    Improved script that updates CR header

--- bin/utility.sh
+++ bin/utility.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# This function find the version tag for a change in a project
 function get_change_tag() {
+    
+
     local project=$1
     local change=$2
 
@@ -10,12 +13,14 @@ function get_change_tag() {
     echo $(grep -A999 -e "^${change}" $project/sqitch.plan | grep -e "^@[23].*" | head -1 | cut -d ' ' -f 1)
 }
 
+# This function reads the sorted list of projects from projects.txt
 function get_projects() {
     local script_dir=$(dirname "$0")
     local projects_file=$(realpath $script_dir/../projects.txt)
     echo $(grep -v '^#' $projects_file | grep -v '^[[:space:]]*$')
 }
 
+# This function finds the project that contains a specific change
 function find_project() {
     local cr=$1
     local projects=$(get_projects)
@@ -27,6 +32,7 @@ function find_project() {
     done
 }
 
+# This function retrieves the issue url for a change in a project plan.
 function get_cr_issue_url() {
     local project=$1
     local change=$2
@@ -41,11 +47,46 @@ function get_cr_issue_url() {
     echo ${url%:}
 }
 
+# This function retrieves the issue id for a change from the URL found in the project plan.
 function get_cr_issue_id() {
     url=$(get_cr_issue_url $1 $2)
     echo ${url##*/}
 }
 
+# This function retrieves the comment for a change in a project plan (issue url is stripped away).
+function get_cr_comment()
+{
+    local project=$1
+    local change=$2
+    local comment=$(awk -v keyword="$change" '$1 == keyword { match($0, /#(.*)$/); print substr($0, RSTART+1, RLENGTH-1) }' $project/sqitch.plan)
+    if [[ "$comment" == *"https://"* ]]; then
+        comment=$(echo $comment | cut -d':' -f3-)
+        comment=$(echo "$comment" | sed -e 's/^ *//' -e 's/ *$//')
+    fi
+    echo $comment
+}
+
+# This function replaces a line starting with a keyword if found in a file, otherwise adds a line at a give linenumber.
+function replace_or_add_line() {
+    local file=$1
+    local keyword=$2
+    local replacement=$3
+    local linenumber=$4
+
+    set +e
+    grep -q "^$keyword" "$file"
+    if [ $? -eq 0 ]
+    then
+        sed -i "/^${keyword}/c${replacement}" "$file"
+    else
+        sed -i "${linenumber}i\
+${replacement}" "$file"
+    fi
+    set -e
+
+}
+
+# This function updates the header of a change in a project with change name, issue url and comment..
 function update_cr_header(){
     local project=$1
     local change=$2
@@ -59,15 +100,34 @@ function update_cr_header(){
     fi
 
     for type in deploy revert verify ; do
+
         local filename="$project/$type/$change.sql"
-        echo "Updating $filename"
         if [ ! -f $filename ]; then
             echo "Change $filename does not exist"
             exit 64
         fi
-        echo "-- ${type^} $project: $change" > /tmp/$change.sql
-        grep -v -i -e "^-- ${type}" $filename >> /tmp/$change.sql ;
-        mv -f /tmp/$change.sql $filename
+        echo "Updating $filename"
+
+        sed -i -e "/^-- ${type}/Id" $filename
+        sed -i "1i\
+-- ${type^} $project: $change" $filename
+
+        local issue_url=$(get_cr_issue_url $project $change)
+        if [ ! -z "$issue_url" ]; then
+            echo "$issue_url"
+            replace_or_add_line $filename "  Issue" "\ \ Issue         $issue_url" 7
+        fi
+
+        local cr_note=$(get_cr_comment $project $change)
+        if [ ! -z "$cr_note" ]; then
+            echo "$cr_note"
+            replace_or_add_line $filename "  Description" "\ \ Description   $cr_note" 8
+        fi
+
+        change_date=${change:0:8}
+        if [[ $change_date =~ ^[0-9]+$ ]]; then
+            replace_or_add_line $filename "  Date" "\ \ Date          $(date -d "${change_date}" +%Y-%m-%d)" 6
+        fi
     done
 
 }
