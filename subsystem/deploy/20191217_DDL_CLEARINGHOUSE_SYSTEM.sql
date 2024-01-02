b3f72dded0fcda4c04a88ea7a8857e2c4ca6271b
Roger Mähler <roger.mahler@hotmail.com>
2024-01-02 21:09:47 +0100

    typo

--- subsystem/deploy/20191217_DDL_CLEARINGHOUSE_SYSTEM.sql
+++ subsystem/deploy/20191217_DDL_CLEARINGHOUSE_SYSTEM.sql
@@ -23,10 +23,8 @@ grant all privileges on all tables in schema public, sead_utility to clearinghou
 grant all privileges on all sequences in schema public, sead_utility to clearinghouse_worker;
 grant execute on all functions in schema public, sead_utility to clearinghouse_worker;
 
-alter default privileges in schema public, sead_utility
-grant all privileges on tables to clearinghouse_worker;
-alter default privileges in schema public, sead_utility;
-grant all privileges on sequences to clearinghouse_worker;
+alter default privileges in schema public, sead_utility grant all privileges on tables to clearinghouse_worker;
+alter default privileges in schema public, sead_utility grant all privileges on sequences to clearinghouse_worker;
 
 set role clearinghouse_worker;
 /*****************************************************************************************************************************
