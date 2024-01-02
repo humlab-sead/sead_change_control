e9b64699e87853c0c4443a8f744d01820f3d9943
Roger Mähler <roger.mahler@hotmail.com>
2024-01-02 21:47:34 +0100

    Use same naming schema as current serial id.

--- general/deploy/20231123_DDL_BIBLIO_UUID.sql
+++ general/deploy/20231123_DDL_BIBLIO_UUID.sql
@@ -57,6 +57,7 @@ begin;
 do $$
 declare
     v_table_name text;
+    v_uuid_name text;
     v_relation_name text;
     v_id_name text;
     v_sql_add_uuid_template text;
@@ -68,11 +69,9 @@ declare
 begin
     begin
 	
-		/*
         alter table tbl_biblio
-            add column if not exists uuid UUID not null default uuid_generate_v4(),
-            add constraint pk_tbl_biblio_uuid unique (uuid);
-		*/
+            add column if not exists biblio_uuid UUID not null default uuid_generate_v4(),
+            add constraint pk_tbl_biblio_uuid unique (biblio_uuid);
 		
         create table if not exists bibliography_references (
             uuid UUID not null,
@@ -80,43 +79,43 @@ begin
             date_updated timestamp with time zone DEFAULT now(),
             primary key (uuid, biblio_uuid),
 			constraint fk_bibliography_references_tbl_biblio_uuid foreign key (biblio_uuid)
-			  references public.tbl_biblio (uuid) match simple
+			  references public.tbl_biblio (biblio_uuid) match simple
 				on update no action
 				on delete no action
         );
 
         v_sql_add_uuid_template = '
             alter table %1$s
-                add column if not exists uuid UUID not null default uuid_generate_v4(),
-                add constraint pk_%1$s_uuid unique (uuid);
+                add column if not exists %2$s_uuid UUID not null default uuid_generate_v4(),
+                add constraint pk_%1$s_uuid unique (%2$s_uuid);
         ';
 
         v_sql_add_fk_template = '
             alter table %1$s
                 add column if not exists biblio_uuid UUID null,
-                add constraint fk_%1$s_tbl_biblio_uuid foreign key (biblio_uuid)
-                    references public.tbl_biblio (uuid) match simple
+                add constraint fk_%1$s_tbl_biblio_biblio_uuid foreign key (biblio_uuid)
+                    references public.tbl_biblio (biblio_uuid) match simple
                     on update no action
                     on delete no action;
         ';
 
         v_sql_update_fk_template = '
             update %1$s
-            set biblio_uuid = tbl_biblio.uuid
+            set biblio_uuid = tbl_biblio.biblio_uuid
             from tbl_biblio
             where %1$s.biblio_id = tbl_biblio.biblio_id;
         ';
 
         v_sql_insert_by_fk_template = '
-            insert into bibliography_references (uuid, biblio_uuid)
-                select %1$s.uuid, tbl_biblio.uuid
+            insert into bibliography_references (%1$s.%2$s, biblio_uuid)
+                select %1$s.biblio_uuid, tbl_biblio.biblio_uuid
                 from %1$s
                 join tbl_biblio using (biblio_id);
         ';
 
         v_sql_insert_by_table_template = '
             insert into bibliography_references (uuid, biblio_uuid, date_updated)
-                select %1$s.uuid, tbl_biblio.uuid, %2$s.date_updated
+                select %1$s.%4$s, tbl_biblio.biblio_uuid, %2$s.date_updated
                 from %1$s
                 join %2$s using (%3$s)
                 join tbl_biblio using (biblio_id);
@@ -129,27 +128,27 @@ begin
             TODO: Add to bibliography_references
 
         */
-        for v_table_name in (
+        for v_table_name, v_uuid_name in (
             select *
             from (values 
-                ('tbl_ecocode_systems'),
-                ('tbl_dataset_masters'),
-                ('tbl_datasets'),
-                ('tbl_aggregate_datasets'),
-                ('tbl_taxonomy_notes'),
-                ('tbl_methods'),
-                ('tbl_rdb_systems'),
-                ('tbl_text_biology'),
-                ('tbl_text_distribution'),
-                ('tbl_text_identification_keys')
-            ) as t (table_name)
+                ('tbl_aggregate_datasets', 'aggregate_dataset_uuid'),
+                ('tbl_dataset_masters', 'master_set_uuid'),
+                ('tbl_datasets', 'dataset_uuid'),
+                ('tbl_ecocode_systems', 'ecocode_system_uuid'),
+                ('tbl_methods', 'method_uuid'),
+                ('tbl_rdb_systems', 'rdb_system_uuid'),
+                ('tbl_taxonomy_notes', 'taxonomy_notes_uuid'),
+                ('tbl_text_biology', 'biology_uuid'),
+                ('tbl_text_distribution', 'distribution_uuid'),
+                ('tbl_text_identification_keys', 'key_uuid')
+            ) as t (table_name, uuid_name)
         ) loop
 
-            v_sql = format(v_sql_add_uuid_template, v_table_name);
+            v_sql = format(v_sql_add_uuid_template, v_table_name, v_uuid_name);
             raise notice 'Executing: %', v_sql;
             execute v_sql;
 
-            v_sql = format(v_sql_insert_by_fk_template, v_table_name);
+            v_sql = format(v_sql_insert_by_fk_template, v_table_name, v_uuid_name);
             raise notice 'Executing: %', v_sql;
             execute v_sql;
 
@@ -162,23 +161,23 @@ begin
             TODO: Add to bibliography_references
 
         */
-        for v_table_name, v_relation_name, v_id_name in (
+        for v_table_name, v_relation_name, v_id_name, v_uuid_name in (
             select *
             from (values 
-                ('tbl_relative_ages', 'tbl_relative_age_refs', 'relative_age_id'),			                -- empty, rename to tbl_relative_age_references
-                ('tbl_sites', 'tbl_site_references', 'site_id'),                                            -- tbl_site_references
-                ('tbl_sample_groups', 'tbl_sample_group_references', 'sample_group_id'),                    --
-                ('tbl_geochronology', 'tbl_geochron_refs', 'geochron_id'),                                  -- empty, rename to tbl_geochron_references
-                ('tbl_taxonomic_order_systems', 'tbl_taxonomic_order_biblio', 'taxonomic_order_system_id'), -- empty, rename to tbl_taxonomic_order_references?
-                ('tbl_tephras', 'tbl_tephra_refs', 'tephra_id')                                             -- empty
-            ) as t (table_name)
+                ('tbl_relative_ages', 'tbl_relative_age_refs', 'relative_age_id', 'relative_age_uuid'),			                -- empty, rename to tbl_relative_age_references
+                ('tbl_sites', 'tbl_site_references', 'site_id', 'site_uuid'),                                            -- tbl_site_references
+                ('tbl_sample_groups', 'tbl_sample_group_references', 'sample_group_id', 'sample_group_uuid'),                    --
+                ('tbl_geochronology', 'tbl_geochron_refs', 'geochron_id', 'geochron_id'),                                  -- empty, rename to tbl_geochron_references
+                ('tbl_taxonomic_order_systems', 'tbl_taxonomic_order_biblio', 'taxonomic_order_system_id', 'taxonomic_order_system_uuid'), -- empty, rename to tbl_taxonomic_order_references?
+                ('tbl_tephras', 'tbl_tephra_refs', 'tephra_id', 'tephra_uuid')                                             -- empty
+            ) as t (table_name, relation_name, id_name, uuid_name)
         ) loop
 
-            v_sql = format(v_sql_add_uuid_template, v_table_name);
+            v_sql = format(v_sql_add_uuid_template, v_table_name, v_uuid_name);
             raise notice 'Executing: %', v_sql;
             execute v_sql;
 
-            v_sql = format(v_sql_insert_by_table_template, v_table_name, v_relation_name, v_id_name);
+            v_sql = format(v_sql_insert_by_table_template, v_table_name, v_relation_name, v_id_name, v_uuid_name);
             raise notice 'Executing: %', v_sql;
             execute v_sql;
 
@@ -186,16 +185,16 @@ begin
 
         /*
             Add forreign key references to bibliography (UUID)
-            These tables contains attriibutes and relations to other tables
+            These tables are relation tables that also contains attriibutes and/or relations to other tables
         */
 
-        for v_table_name in (
+        for v_table_name, v_id_name, v_uuid_name in (
             select *
             from (values 
-                ('tbl_species_associations'),
-                ('tbl_taxa_synonyms'),
-                ('tbl_site_other_records')
-            ) as t (table_name)
+                ('tbl_site_other_records', 'site_other_records_id', 'site_other_records_uuid'),
+                ('tbl_species_associations', 'species_association_id', 'species_association_uuid'),
+                ('tbl_taxa_synonyms', 'synonym_id', 'synonym_uuid')
+            ) as t (table_name, id_name, uuid_name)
         ) loop
 
             v_sql = format(v_sql_add_fk_template, v_table_name);
@@ -205,6 +204,8 @@ begin
             v_sql = format(v_sql_update_fk_template, v_table_name);
             raise notice 'Executing: %', v_sql;
             execute v_sql;
+
+            -- FIXME: Should we also add records to bibliography_references?
 
         end loop;
 	end;
