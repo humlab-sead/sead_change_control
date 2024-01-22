6569914303fbce9c65dc31cb0635f35ccd98e37f
Roger Mähler <roger.mahler@hotmail.com>
2024-01-22 15:18:47 +0100

    Bugs import data Dec 2023 (#162)

--- bugs/deploy/20231211_DML_SUBMISSION_BUGS_20231219_COMMIT.sql
+++ bugs/deploy/20231211_DML_SUBMISSION_BUGS_20231219_COMMIT.sql
@@ -4,16 +4,16 @@
   Date          2023-12-11
   Description   Full (inital) non-incremental import of BugsCEP data version 20230705.
   Issue         https://github.com/humlab-sead/sead_change_control/issues/162
-  Prerequisites 
-  Reviewer      
-  Approver      
+  Prerequisites
+  Reviewer
+  Approver
   Idempotent    Yes
   Notes
 *****************************************************************************************************************/
 
+set client_encoding = 'UTF8';
 set client_min_messages to warning;
 \set autocommit off;
-\t
 
 set search_path TO public;
 
@@ -22,8 +22,12 @@ do $$ begin
     perform sead_utility.set_fk_is_deferrable('public', true, false);
 end $$ language plpgsql;
 
-\ir 20231211_DML_SUBMISSION_BUGS_20231219_COMMIT/sead_staging_202212_vs_sead_staging_bugs_DEPLOY_public_202401031734.sql
-\ir 20231211_DML_SUBMISSION_BUGS_20231219_COMMIT/sead_staging_202212_vs_sead_staging_bugs_DEPLOY_bugs_import_202401031724.sql
+begin;
+
+set constraints all deferred;
+
+\ir 20231211_DML_SUBMISSION_BUGS_20231219_COMMIT/public.sql
+\ir 20231211_DML_SUBMISSION_BUGS_20231219_COMMIT/bugs_import.sql
 
 commit;
 
