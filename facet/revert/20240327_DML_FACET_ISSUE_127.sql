bce744bd98b55406d5c28bba74d0963a5c9efccb
Roger Mähler <roger.mahler@hotmail.com>
2024-03-27 13:25:53 +0100

    Resolves https://github.com/humlab-sead/sead_change_control/issues/127

---
+++ facet/revert/20240327_DML_FACET_ISSUE_127.sql
@@ -0,0 +1,7 @@
+-- Revert sead_api:20240327_DML_FACET_ISSUE_127 from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ facet/verify/20240327_DML_FACET_ISSUE_127.sql
@@ -0,0 +1,7 @@
+-- Verify sead_api:20240327_DML_FACET_ISSUE_127 on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
