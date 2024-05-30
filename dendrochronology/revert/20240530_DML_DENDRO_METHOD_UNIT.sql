9e6ab727a2d1617273464d26ac425f53cd7b84f3
Roger Mähler <roger.mahler@hotmail.com>
2024-05-30 13:51:36 +0200

    Resolves #276 and #281

---
+++ dendrochronology/revert/20240530_DML_DENDRO_METHOD_UNIT.sql
@@ -0,0 +1,7 @@
+-- Revert dendrochronology:20240530_DML_DENDRO_METHOD_UNIT from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ dendrochronology/verify/20240530_DML_DENDRO_METHOD_UNIT.sql
@@ -0,0 +1,7 @@
+-- Verify dendrochronology:20240530_DML_DENDRO_METHOD_UNIT on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
