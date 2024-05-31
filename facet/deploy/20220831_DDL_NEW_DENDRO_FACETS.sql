45db9777162eab66b5374ce3ac60b01e15bcf285
Roger Mähler <roger.mahler@hotmail.com>
2024-05-31 15:16:33 +0200

    Cleaned up script (#118, #269)

--- facet/deploy/20220831_DDL_NEW_DENDRO_FACETS.sql
+++ facet/deploy/20220831_DDL_NEW_DENDRO_FACETS.sql
@@ -1,159 +1,139 @@
--- Deploy sead_api: 20220831_DDL_NEW_DENDRO_FACETS
--- Active: 1661931903212@@humlabseadserv.srv.its.umu.se@5432@sead_development@facet
+-- Deploy facet: 20240403_DML_RANGES_INTERSECT_FACET
 
 /****************************************************************************************************************
   Author        Roger Mähler
-  Date          2022-08-31
-  Issue         https://github.com/humlab-sead/sead_change_control/issues/91
-  Description   New dendro facets (see issue #91)
-  Prerequisites
-  Reviewer
-  Approver
+  Date          2024-04-03
+  Description   See https://github.com/humlab-sead/sead_query_api/issues/118 and https://github.com/humlab-sead/sead_query_api/issues/115
+  Issue         https://github.com/humlab-sead/sead_change_control/issues/269
+  Prerequisites 
+  Reviewer      
+  Approver      
   Idempotent    Yes
   Notes
 *****************************************************************************************************************/
-
--- create or replace function facet.delete_facet(i_facet_id int) returns bool language 'plpgsql'
--- as $body$
--- 	declare j_tables json;
--- 	declare j_clauses json;
--- 	declare s_facet_code text;
--- 	declare i_aggregate_facet_id int = 0;
--- begin
-
--- 	s_facet_code = (select max(facet_code) from facet.facet where facet_id = i_facet_id);
--- 	raise notice 'facet code: %', i_facet_id;
-
--- 	if s_facet_code is null then
--- 		raise notice 'facet not found %', i_facet_id;
--- 		return false;
--- 	end if;
-
--- 	delete from facet.facet_clause where facet_id = i_facet_id;
--- 	delete from facet.facet_table where facet_id = i_facet_id;
--- 	delete from facet.facet_children where s_facet_code in (facet_code, child_facet_code);
--- 	delete from facet.facet_dependency where i_facet_id in (facet_id, dependency_facet_id);
--- 	delete from facet.facet where facet_id = i_facet_id;
-
--- 	return true;
-
--- end
--- $body$;
-
--- alter function facet.delete_facet(int)
---     owner to humlab_admin;
-
--- grant execute on function facet.delete_facet(int) to humlab_admin;
-
--- select facet.delete_facet(48);
-
-/*
-select group_description, count(distinct analysis_entity_id)
-from tbl_sites s
-join tbl_sample_groups sg using (site_id)
-join tbl_sample_group_descriptions sgd using (sample_group_id)
-join tbl_sample_group_description_types sgdt using (sample_group_description_type_id)
-join tbl_physical_samples ps using (sample_group_id)
-join tbl_analysis_entities ae using (physical_sample_id)
-where sgd.sample_group_description_type_id = 60
-group by group_description;
-*/
 
 begin;
 do $$
 declare s_facets text;
 declare j_facets jsonb;
-declare i_next_position int;
 begin
 
-    begin
+    set client_encoding = 'UTF8';
 
-        set search_path = facet, pg_catalog;
-        set client_encoding = 'UTF8';
+    if not exists (select 1 from facet.facet_type where facet_type_id = 4) then
+        insert into facet.facet_type (facet_type_id, facet_type_name, reload_as_target)
+            values (4, 'rangesintersect', TRUE);
+    end if;
 
-		s_facets = $facets$
-        [
+    alter table public.tbl_analysis_entity_ages
+        drop column if exists age_range;
 
-            {
-                "facet_id": 48,
-                "facet_code": "construction_type",
-                "display_title": "Construction types",
-                "description": "Construction types",
-                "facet_group_id": "1",
-                "facet_type_id": "1",
-                "category_id_expr": "tbl_sample_group_descriptions.sample_group_description_id",
-                "category_id_type": "integer",
-                "category_name_expr": "tbl_sample_group_descriptions.group_description || ' ' || tbl_sample_groups.sample_group_name",
-                "sort_expr": "tbl_sample_group_descriptions.group_description",
-                "is_applicable": true,
-                "is_default": false,
-                "aggregate_type": "count",
-                "aggregate_title": "Number of samples",
-                "aggregate_facet_code": "result_facet",
-                "tables": [
-                    {
-                        "sequence_id": 1,
-                        "table_name": "tbl_sample_groups",
-                        "udf_call_arguments": null,
-                        "alias":  null
-                    },
-                    {
-                        "sequence_id": 2,
-                        "table_name": "tbl_sample_group_descriptions",
-                        "udf_call_arguments": null,
-                        "alias":  null
-                    },
-                    {
-                        "sequence_id": 3,
-                        "table_name": "tbl_sample_group_description_types",
-                        "udf_call_arguments": null,
-                        "alias":  null
-                    },
-                    {
-                        "sequence_id": 4,
-                        "table_name": "tbl_sites",
-                        "udf_call_arguments": null,
-                        "alias":  null
-                    },
-                    {
-                        "sequence_id": 5,
-                        "table_name": "tbl_physical_samples",
-                        "udf_call_arguments": null,
-                        "alias":  null
-                    }
-                ],
-                "clauses": [
-                    {
-                        "clause": "tbl_sample_group_descriptions.sample_group_description_type_id=60",
-                        "enforce_constraint": true
-                    } ]
-            }
-        ]
+	/* BP (Before Present), age_older >= age_younger */
+    alter table public.tbl_analysis_entity_ages
+        add column age_range int4range null
+            generated always as (
+                case when age_younger is null and age_older is null then null
+                else int4range(
+                    coalesce(age_younger::int, age_older::int),
+                    coalesce(age_older::int, age_younger::int) + 1
+                )
+            	end) stored;
+
+
+    alter table public.tbl_dendro_dates
+        drop column if exists age_range;
+
+	/* BCE/CE age_older <= age_younger */
+    alter table public.tbl_dendro_dates
+        add column age_range int4range null
+            generated always as (
+                case when age_younger is null and age_older is null then null
+                else int4range(
+                    coalesce(age_older::int, age_younger::int),
+                    coalesce(age_younger::int, age_older::int) + 1
+                )
+            	end) stored;
+
+	alter table facet.facet drop column if exists category_id_operator;
+
+    alter table facet.facet
+        add column if not exists category_id_operator varchar(40) not null DEFAULT ('=');
+
+    s_facets = $facets$
+    [
+        {
+            "facet_id": 52,
+            "facet_code": "analysis_entity_ages",
+            "display_title": "Analysis entity ages",
+            "description": "Analysis entity ages (intersects)",
+            "facet_group_id":"2",
+            "facet_type_id": 4,
+            "category_id_expr": "age_range",
+            "category_id_type": "int4range",
+            "category_id_operator": "&&",
+            "category_name_expr": "age_range",
+            "sort_expr": "1",
+            "is_applicable": true,
+            "is_default": true,
+            "aggregate_type": "count",
+            "aggregate_title": "Number of samples",
+            "aggregate_facet_code": "result_facet",
+            "tables": [
+                {
+                    "sequence_id": 1,
+                    "table_name": "tbl_analysis_entity_ages",
+                    "udf_call_arguments": null,
+                    "alias":  null
+                } ],
+            "clauses": [  ]
+        },
+        {
+            "facet_id": 53,
+            "facet_code": "dendro_age_contained_by",
+            "display_title": "Dendro chronology age ranges",
+            "description": "Dendrochronology ages (contained by)",
+            "facet_group_id":"2",
+            "facet_type_id": 4,
+            "category_id_expr": "tbl_dendro_dates.age_range",
+            "category_id_type": "int4range",
+            "category_id_operator": "<@",
+            "category_name_expr": "tbl_dendro_dates.age_range",
+            "sort_expr": "1",
+            "is_applicable": true,
+            "is_default": true,
+            "aggregate_type": "count",
+            "aggregate_title": "Number of samples",
+            "aggregate_facet_code": "result_facet",
+            "tables": [
+                {
+                    "sequence_id": 1,
+                    "table_name": "tbl_dendro_dates",
+                    "udf_call_arguments": null,
+                    "alias":  null
+                } ],
+            "clauses": [  ]
+        }
+    ]
 
 $facets$;
 
-        j_facets = s_facets::jsonb;
+    j_facets = s_facets::jsonb;
 
-        perform facet.create_or_update_facet(v.facet::jsonb)
-        from jsonb_array_elements(j_facets) as v(facet);
-
-
-    i_next_position = coalesce((select max(position) + 1 from facet.facet_children where facet_code = 'dendrochronology'), 0);
+    perform facet.create_or_update_facet(v.facet::jsonb)
+    from jsonb_array_elements(j_facets) as v(facet);
 
 	insert into facet.facet_children (facet_code, child_facet_code, position)
 		select 'dendrochronology', facet_code, position
 		from facet.facet
 		join (values
-			('construction_type', i_next_position + 0)
+			('dendro_age_contained_by', coalesce((select max(position) + 1 from facet.facet_children where facet_code = 'dendrochronology'), 0) + 0)
 		) as v(facet_code, position) using (facet_code)
 		where is_applicable = TRUE
           on conflict (facet_code, child_facet_code)
           do nothing
           ;
 
-    exception when sqlstate 'GUARD' then
-        raise notice 'ALREADY EXECUTED';
-    end;
-
 end $$;
 commit;
+
+
