29dcc47fb9c0d6b23d2a8630edf2dcb13a988b4e
Roger Mähler <roger.mahler@hotmail.com>
2024-05-31 12:40:19 +0200

    Deprecated old ceramics/dendro tables. Resolves 20240530_DDL_UDF_DROP_DEPRECATED_TABLES: Deprecate tables #70.

---
+++ sead_model/revert/20240530_DDL_UDF_DROP_DEPRECATED_TABLES.sql
@@ -0,0 +1,7 @@
+-- Revert sead_model:20240530_DDL_UDF_DROP_DEPRECATED_TABLES from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ sead_model/verify/20240530_DDL_UDF_DROP_DEPRECATED_TABLES.sql
@@ -0,0 +1,7 @@
+-- Verify sead_model:20240530_DDL_UDF_DROP_DEPRECATED_TABLES on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
