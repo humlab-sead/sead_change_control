6d39307496eac8b67d3a4bdd9a34f23bb53ef320
Roger Mähler <roger.mahler@hotmail.com>
2024-06-01 11:11:05 +0200

    Deprecated CR (#269) Moved to other CRs.

---
+++ facet/archive/deploy/20220831_DDL_NEW_DENDRO_FACETS.sql
@@ -0,0 +1,13 @@
+-- Deploy facet: 20240403_DML_RANGES_INTERSECT_FACET
+
+/****************************************************************************************************************
+  Author        Roger Mähler
+  Date          2024-04-03
+  Description   See https://github.com/humlab-sead/sead_query_api/issues/118 and https://github.com/humlab-sead/sead_query_api/issues/115
+  Issue         https://github.com/humlab-sead/sead_change_control/issues/269
+  Prerequisites 
+  Reviewer      
+  Approver      
+  Idempotent    Yes
+  Notes
+*****************************************************************************************************************/
