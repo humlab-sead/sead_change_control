f53748b55e7ef18fd388f57c3d352ce494eca5d0
Roger Mähler <roger.mahler@hotmail.com>
2024-06-04 11:30:05 +0200

    Resolves #301.

---
+++ facet/revert/20240604_DML_SAMPLE_GROUP_FACET.sql
@@ -0,0 +1,7 @@
+-- Revert sead_api:20240604_DML_SAMPLE_GROUP_FACET from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ facet/verify/20240604_DML_SAMPLE_GROUP_FACET.sql
@@ -0,0 +1,7 @@
+-- Verify sead_api:20240604_DML_SAMPLE_GROUP_FACET on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
