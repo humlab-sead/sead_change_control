541220fabf4b08b5e0347c90fb01363ef047e053
Roger Mähler <roger.mahler@hotmail.com>
2024-10-25 09:25:08 +0200

    data: new dendro lookup data (#317)

---
+++ dendrochronology/revert/20241023_DML_NEW_DENDRO_LOOKUPS.sql
@@ -0,0 +1,7 @@
+-- Revert dendrochronology:20241023_DML_NEW_DENDRO_LOOKUPS from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ dendrochronology/verify/20241023_DML_NEW_DENDRO_LOOKUPS.sql
@@ -0,0 +1,7 @@
+-- Verify dendrochronology:20241023_DML_NEW_DENDRO_LOOKUPS on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
