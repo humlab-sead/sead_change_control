8367c445904fa393c469bcc72186bfeccb13d227
Roger Mähler <roger.mahler@hotmail.com>
2024-12-12 11:06:05 +0100

    refactor: update SQL script for schema creation and command execution

--- subsystem/deploy/20191217_DDL_CLEARINGHOUSE_SYSTEM.sql
+++ subsystem/deploy/20191217_DDL_CLEARINGHOUSE_SYSTEM.sql
@@ -10,9 +10,9 @@
   Idempotent    Yes
 *****************************************************************************************************************/
 
-set client_min_messages to warning;
-
-create schema if not exists clearing_house authorization clearinghouse_worker;
+set client_encoding = 'UTF8';
+set standard_conforming_strings = on;
+set client_min_messages = warning;
 
 alter user clearinghouse_worker createdb;
 
@@ -24,7 +24,12 @@ grant execute on all functions in schema public, sead_utility to clearinghouse_w
 alter default privileges in schema public, sead_utility grant all privileges on tables to clearinghouse_worker;
 alter default privileges in schema public, sead_utility grant all privileges on sequences to clearinghouse_worker;
 
+drop schema if exists clearing_house cascade;
+
+create schema if not exists clearing_house authorization clearinghouse_worker;
+
 set role clearinghouse_worker;
+
 
 \set autocommit off;
 
@@ -32,18 +37,19 @@ set role clearinghouse_worker;
 
 begin;
 
-\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/01_clearinghouse_model.sql
-\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/02_utility.sql
-\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/03_populate_clearinghouse_model.sql
-\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/04_entity_model.sql
+
+\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/01_clearinghouse_model.sql;
+\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/02_utility.sql;
+\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/03_populate_clearinghouse_model.sql;
+\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/04_entity_model.sql;
 
 call clearing_house.create_clearinghouse_model(false);
 call clearing_house.populate_clearinghouse_model();
 call clearing_house.create_public_model(false, false);
 
-\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/06_extract_and_resolve.sql
-\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/07_review.sql
-\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/08_reporting.sql
+\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/06_extract_and_resolve.sql;
+\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/07_review.sql;
+\i 20191217_DDL_CLEARINGHOUSE_SYSTEM/08_reporting.sql;
 
 commit;
 
