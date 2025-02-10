f9dab9c14f81b1f8ab1650fe25b13af49ae9c146
Roger Mähler <roger.mahler@hotmail.com>
2025-02-10 09:37:09 +0100

    change: Backported change 20240104_DDL_DATE_SUBMITTED_TYPE (#228) to SEAD model.

--- sead_model/deploy/20240104_DDL_DATE_SUBMITTED_TYPE.sql
+++ sead_model/deploy/20240104_DDL_DATE_SUBMITTED_TYPE.sql
@@ -15,18 +15,19 @@
 begin;
 do $$
 begin
+	raise notice '20240104_DDL_DATE_SUBMITTED_TYPE has been backported to SEAD model and is now idempotent';
 
-  if exists (
-    select 1
-    from information_schema.columns
-    where table_schema = 'public'
-      and table_name = 'tbl_dataset_submissions'
-      and column_name = 'date_submitted'
-      and data_type != 'text'
-  ) then
-      alter table tbl_dataset_submissions alter column "date_submitted" type text using "date_submitted"::text;
-		  alter table tbl_dataset_submissions alter column "date_submitted" drop not null;
-  end if;
+  -- if exists (
+  --   select 1
+  --   from information_schema.columns
+  --   where table_schema = 'public'
+  --     and table_name = 'tbl_dataset_submissions'
+  --     and column_name = 'date_submitted'
+  --     and data_type != 'text'
+  -- ) then
+  --     alter table tbl_dataset_submissions alter column "date_submitted" type text using "date_submitted"::text;
+	-- 	  alter table tbl_dataset_submissions alter column "date_submitted" drop not null;
+  -- end if;
 
 end $$;
 commit;
