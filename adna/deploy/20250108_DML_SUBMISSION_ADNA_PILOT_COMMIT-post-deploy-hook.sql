3479f470fcad71630fe1e1a3c4f7366599da3aa7
Roger Mähler <roger.mahler@hotmail.com>
2026-02-01 12:41:24 +0100

    feat: add post-deploy hook for ancient DNA dataset insertion

---
+++ adna/deploy/20250108_DML_SUBMISSION_ADNA_PILOT_COMMIT-post-deploy-hook.sql
@@ -0,0 +1,3 @@
+insert into tbl_datasets(method_id,biblio_id,date_updated,description,method_abbrev_or_alt_name,method_group_id,method_name,record_type_id,unit_id,method_uuid)
+   values (
+175,NULL,2025-04-24 08:34:07.182333+00,"Extraction and analysis of ancient DNA recovered from biological material (human, animal, plants, etc.)",aDNA,1,Ancient DNA analysis,22,NULL,9052de6a-93aa-4310-b7fc-83b9c75f3ccb
