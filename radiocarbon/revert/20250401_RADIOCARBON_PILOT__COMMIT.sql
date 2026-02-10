59194bdb0f38804c8719420f793c3458df38c1bb
Roger Mähler <roger.mahler@hotmail.com>
2026-02-10 14:37:11 +0100

    feat: iniial commit for radiocarbon pilot data

---
+++ radiocarbon/revert/20250401_RADIOCARBON_PILOT__COMMIT.sql
@@ -0,0 +1,7 @@
+-- Revert radiocarbon:20250401_RADIOCARBON_PILOT__COMMIT from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ radiocarbon/verify/20250401_RADIOCARBON_PILOT__COMMIT.sql
@@ -0,0 +1,7 @@
+-- Verify radiocarbon:20250401_RADIOCARBON_PILOT__COMMIT on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
